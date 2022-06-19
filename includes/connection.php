fd1ee2af5b343eecf2bda2254631fe034fc1f5e8
Mumbere Godwin <godwinmumbere3@gmail.com>
2022-06-19 19:27:07 +0300

    cc

--- includes/connection.php
+++ includes/connection.php
@@ -1,11 +1,11 @@
 <?php      
     $host = "localhost";  
-    $user = "admin";  
-    $passw = 'admin';  
-    $dbname = "mugisha";  
+    $user = "julyhost_mugisha";  
+    $password = 'PhI.TA8auV{t';  
+    $dbname = "julyhost_mugisha";  
       
-    $con = mysqli_connect($host, $user, $passw, $dbname);  
-    if( mysqli_connect_errno()) {  
+    $con = mysqli_connect($host, $user, $password, $dbname);  
+    if(mysqli_connect_errno()) {  
         die("Failed to connect with MySQL: ". mysqli_connect_error());  
     }  
 ?>   
