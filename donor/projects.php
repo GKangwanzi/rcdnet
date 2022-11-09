4bfe339126f3a2f26fdb479da77f0cd01d40ff5e
Mumbere Godwin <godwinmumbere3@gmail.com>
2022-11-09 10:50:16 +0300

    donor projects

--- donor/projects.php
+++ donor/projects.php
@@ -14,12 +14,9 @@
         </div>
     </div>
 
-
     <?php 
     include "includes/sidebarmenu.php";
     ?>
-
-
     <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
     </div>
         </div>
@@ -51,26 +48,47 @@
                         <br>
                         <!-- table hover -->
                         <div class="table-responsive" style="padding-right: 20px; padding-left: 20px;">
-                            <table class="table table-striped" id="table1">
-                    <thead>
-                        <tr>
-                            <th>PROJECT NAME</th>
-                            <th>START</th>
-                            <th>END</th>
-                            <th>OFFICER</th>
-                            <th>STATUS</th>
-                            <th>ACTION</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
+                            <?php
+include "../includes/connection.php";
 
-                    </tbody>
-                </table>
+$sql = "SELECT * FROM project"; 
+if($result = mysqli_query($con, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table class='table table-striped' id='table5'>";
+            echo "<thead>";
+             echo "<tr>";
+                echo "<th>Name</th>";
+                echo "<th>Start Date</th>";
+                echo "<th>End Date</th>";
+                echo "<th>Status</th>";
+                echo "<th>Action</th>";
+            echo "</tr>";
+            echo "</thead>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['startdate'] . "</td>";
+                echo "<td>" . $row['stopdate'] . "</td>";
+                echo "<td>" . "<a class='badge bg-info'>". $row['status'] ."</a>
+                ". "</td>";
+                echo "<td>" . "<a href='projectdetail.php?id=".$row['prID']."' class='badge bg-success'>View Details</a>
+                ". "</td>";
+            echo "</tr>"; 
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+?>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> 
         </div>
     </section>
     <!-- Hoverable rows end -->
@@ -78,16 +96,26 @@
     </section>
 </div>
 
-            <footer>
-                <div class="footer clearfix mb-0 text-muted">
-                    <div class="float-start">
-                        <p>Created by <a href="http://julybrands.co.ug">JulyBrands Digital</a></p>
-                    </div>
-                </div>
-            </footer>
+    <footer>
+        <div class="footer clearfix mb-0 text-muted">
+            <div class="float-start">
+                <p>Created by <a href="http://julybrands.co.ug">JulyBrands Digital</a></p>
+            </div>
         </div>
+    </footer>
+</div>
     </div>
 <?php include "includes/scripts.php"; ?>
 </body>
+<script>
+    // Simple Datatable
+    let table5 = document.querySelector('#table5');
+    let dataTable = new simpleDatatables.DataTable(table5);
+</script>
 
+<script type="text/javascript">
+    function DeleteConfirm() {
+      return confirm("Are you sure to delete this project");
+     }
+ </script>
 </html>
